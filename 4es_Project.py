7cec9dcf8859ca6256b793759ec66af30fbba9b8
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-03-30 11:01:41 +0200

    changing in images size

--- 4es_Project.py
+++ 4es_Project.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 
 imageSource = cv2.imread('venv/lib/images/4es.png')
-imageSource = cv2.resize(imageSource, (0, 0), fx=0.6, fy=0.6)
+imageSource = cv2.resize(imageSource, (0, 0), fx=0.4, fy=0.4)
 imageSource = cv2.cvtColor(imageSource, cv2.IMREAD_COLOR)
 
 templateImage = cv2.imread('venv/lib/images/A.png')
-templateImage = cv2.resize(templateImage, (0, 0), fx=0.4, fy=0.4)
+templateImage = cv2.resize(templateImage, (0, 0), fx=0.7, fy=0.8)
 templateImage = cv2.cvtColor(templateImage, cv2.IMREAD_COLOR)
 
 
@@ -19,7 +19,7 @@ for method in methods:
 
     result = cv2.matchTemplate(newImageSource, templateImage, method)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    if method in [cv2.TM_CCOEFF_NORMED]:
+    if method in [ cv2.TM_CCOEFF_NORMED]:
         location = min_loc
     else:
         location = max_loc
